7fb455dbc54a913ea6e71dae6acaf9235dcd9c9b
Ishwar <ishwardevhare@gmail.com>
2025-12-29 22:14:27 +0530

    04- Edit and complete button functionality

--- gui_todo/gui.py
+++ gui_todo/gui.py
@@ -7,12 +7,13 @@ addButton = sg.Button('Add')
 
 list_box = sg.Listbox(values=functions.get_todos(), key='todos', enable_events=True, size=[45,10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
 
 exitButton = sg.Button("Exit")
 
 layout = [[label],
           [input, addButton],
-          [list_box, edit_button],
+          [list_box, edit_button, complete_button],
           [exitButton]]
 
 font = ('Tahoma', 20)
@@ -26,7 +27,9 @@ while True:
             todos = functions.get_todos()
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
-            functions.write_todos(todos)
+            functions.write_todos(todos)            
+            window['todos'].update(values = todos)
+            window['todo'].update(value='')
         case "Edit":
             todo_to_edit = values['todos'][0]
             new_todo = values['todo']
@@ -35,9 +38,16 @@ while True:
             index = todos.index(todo_to_edit)
             todos[index] = new_todo
             functions.write_todos(todos)
-
+            window['todo'].update(value='')
+        case "Complete":
+            todo_to_complete = values["todos"][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
             window['todos'].update(values = todos)
-        
+            window['todo'].update(value='')
+        case "Exit":
+            break
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
